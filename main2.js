0e3f9dd2d910acb7ae208220945d4ad815876805
mcalvi84 <86073164+mcalvi84@users.noreply.github.com>
2022-08-24 22:16:17 -0300

    Agregue un archivo externo .js
    Para no borrar lo que venia haciendo con los promp, ahora arme uno paralelo! Con los eventos, me falta arreglar el tema id... se puede borrar y agregar lineas nuevas.. desde agregar y borrar.

---
+++ main2.js
@@ -0,0 +1,57 @@
+var selectedRow = null;
+//Mostrar Alertas
+function showAlert(message, className) {
+  const div = document.createElement("div");
+  div.className = `alert alert-${className}`;
+
+  div.appendChild(document.createTextNode(message));
+  const container = document.querySelector(".container");
+  const main = document.querySelector(".main");
+  container.insertBefore(div, main);
+
+  setTimeout(() => document.querySelector(".alert").remove(), 3000);
+}
+
+//Borrar Datos
+
+document.querySelector("#producto_lista").addEventListener("click", (e) => {
+  target = e.target;
+  if (target.classList.contains("delete")) {
+    target.parentElement.parentElement.remove();
+    showAlert("Producto Borrado con Exito", "danger");
+  }
+});
+
+//Añadir Datos
+
+document.querySelector("#producto_form").addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  //agarrar los valores
+
+  const producto = document.querySelector("#producto").value;
+  const cantidad = document.querySelector("#cantidad").value;
+  const precio = document.querySelector("#precio").value;
+
+  //validar
+  if (producto == "" || cantidad == "" || precio == "") {
+    showAlert("Por favor complete todos los campos", "danger");
+  } else {
+    if (selectedRow == null) {
+      const list = document.querySelector("#producto_lista");
+      const row = document.createElement("tr");
+
+      row.innerHTML = `
+         <td>${producto}</td>
+         <td>${cantidad}</td>
+         <td>${precio}</td>
+         <td>
+         <a href="" class="btn btn-warning btn-sm edit">Editar</a>
+         <a href="" class="btn btn-danger btn-sm delete">Borrar</a>
+        `;
+      list.appendChild(row);
+      selectedRow = null;
+      showAlert("Producto añadido Exitosamente", "succes");
+    }
+  }
+});
